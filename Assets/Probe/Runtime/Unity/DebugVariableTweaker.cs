460501d054335395e35caa420ef728e18081d541
Gaba <gabrielfpaula@gmail.com>
2026-02-04 21:06:13 -0300

    🚚 Rename variable tweaker

---
+++ Assets/Probe/Runtime/Unity/DebugVariableTweaker.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Probe.Runtime.Unity
+{
+    public class DebugVariableTweaker : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
